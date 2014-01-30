3f4af25acc607a601db92c7856b30745817a84fe
Jordan <jordan.baucke@gmail.com>
2014-01-30 15:22:44 -0700

    initial

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -8,7 +8,4 @@ import org.springframework.context.annotation.ComponentScan;
 @EnableAutoConfiguration
 public class Application {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
 }
