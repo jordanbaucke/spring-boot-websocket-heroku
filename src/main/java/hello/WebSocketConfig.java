443102c0128f4f4e27de5477f6aeb13f6a597e9d
Jordan <jordan.baucke@gmail.com>
2014-01-30 15:12:02 -0700

    initial

--- src/main/java/hello/WebSocketConfig.java
+++ src/main/java/hello/WebSocketConfig.java
@@ -6,11 +6,11 @@ import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurationSupport;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
-@EnableWebSocketMessageBroker
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+public class WebSocketConfig extends WebSocketMessageBrokerConfigurationSupport{
 
 	@Override
 	public void configureMessageBroker(MessageBrokerRegistry config) {
